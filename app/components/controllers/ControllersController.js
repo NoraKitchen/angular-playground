7b92cccd75bc5bdb5e02d0c07cc998bd55a8ccd2
Nora Kitchen <narwhaltortellini@gmail.com>
2016-04-25 17:25:23 -0600

    finished repeating

--- app/components/controllers/ControllersController.js
+++ app/components/controllers/ControllersController.js
@@ -5,4 +5,13 @@ app.controller('ControllersController', ControllersController);
 
 function ControllersController(){
 	this.controllersStatus = 'Working';
+    var controller = this;
+    
+    controller.friends = [];
+    
+    controller.addFriend = function(){
+        controller.friends.push(controller.newFriend);
+        controller.newFriend = "";
+    }
+    
 }
